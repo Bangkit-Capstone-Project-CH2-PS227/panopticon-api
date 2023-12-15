dbde04c66dc41717806ea880274855d1d3d96e19
Bhedil <aidilarifinnizar@gmail.com>
2023-12-15 15:19:34 +0700

    change refreshToken and userModel

---
+++ Backend/routes/routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { getUsers, login, register, logout, createRoom, landingPage } from "../controllers/users.js";
+//Memverifikasi endpoint yang tidak diakses apabila user tidak login
+import { verifyToken } from "../middleware/verifyToken.js";
+import { refreshToken } from "../controllers/refreshToken.js";
+
+const router = express.Router();
+
+router.get('/users', verifyToken, getUsers);
+router.post('/users', register);
+router.post('/login', login);
+router.get('/token', refreshToken)
+router.delete('/logout', logout)
+router.post('/room', createRoom)
+router.get('/', landingPage)
+
+export default router;
